c61246d579e1b55417cc905386f23b1a40aec3cb
nanakawano <115549602+nanakawano@users.noreply.github.com>
2022-12-09 00:02:37 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let name = "Nana" ;
+console.log(name) 
+
+let age = 23;
+console.log(age)
+
+console.log(7 * age)
+
+let friends = ["Débora", "Leticia", "Luana"];
+let numbers = [1, 2, 3];
+console.log(friends[1]);
+
+let person = {
+    name: "Nana",
+    age: 23,
+    isStudent: true
+  };
+  console.log(person.name);
+
+let students = [
+    {
+      name: "Débora",
+      age: 18,
+      isStudent: true
+    },
+    {
+      name: "Leticia",
+      age: 19,
+      isStudent: true
+    },
+    {
+      name: "Luana",
+      age: 18,
+      isStudent: false
+    }
+  ];
+  console.log(students[0].name);
